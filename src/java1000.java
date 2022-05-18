d76c441f23085757eaa64a2354beba1a8f35890d
ke2007 <dlawnsrjs@gmail.com>
2022-05-18 12:28:17 +0900

    📝 : 백 준  1 0 0 0 번
    Scanner  를  BufferedReader 로  변 경
    
    Resolves(해결 이슈): #1 :: bufferedreader 로 변경
    Ref(참고할만한 이슈) : Null.
    Related to(아직 해결 못한 이슈) : Null.

--- src/java1000.java
+++ src/java1000.java
@@ -5,17 +5,12 @@ import java.util.Scanner;
 
 public class java1000 {
     public static void main(String[] args) throws IOException {
+
         BufferedReader sc2 = new BufferedReader(new InputStreamReader(System.in));
-
+        String[] strs = sc2.readLine().split(" ");
         int a,b;
-        a = Integer.parseInt(sc2.readLine());
-        b = Integer.parseInt(sc2.readLine());
-
-        try {
-            sc2.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        a = Integer.parseInt(strs[0]);
+        b = Integer.parseInt(strs[1]);
         System.out.println(a+b);
 
     }
