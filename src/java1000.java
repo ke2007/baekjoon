8972b6c1ca9c8899646ff2d203431aa3c9680dfe
ke2007 <dlawnsrjs@gmail.com>
2022-05-18 11:50:06 +0900

    백준 자바 1000

--- src/java1000.java
+++ src/java1000.java
@@ -1,2 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class java1000 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader sc2 = new BufferedReader(new InputStreamReader(System.in));
+
+        int a,b;
+        a = Integer.parseInt(sc2.readLine());
+        b = Integer.parseInt(sc2.readLine());
+
+        try {
+            sc2.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(a+b);
+
+    }
 }
