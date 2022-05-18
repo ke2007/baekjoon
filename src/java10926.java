9977d837b1e9a5ff8a497a3a496356619bdb04c4
ke2007 <dlawnsrjs@gmail.com>
2022-05-18 12:23:43 +0900

    백준 자바 10926

---
+++ src/java10926.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Objects;
+
+public class java10926 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String[] strs = bf.readLine().split(" ");
+
+            System.out.println(strs[0] + "??!");
+
+    }
+}
