218a8c5e6d2200ec5b9a4843fd7f545a2facc8d9
dlawn <dlawnsrjs@gmail.com>
2022-06-19 16:44:56 +0900

    4673

--- src/java4673.java
+++ src/java4673.java
@@ -2,6 +2,31 @@ import java.io.BufferedReader;
 
 public class java4673 {
     public static void main(String[] args) {
-        BufferedReader
+
+        boolean[] a = new boolean[10001];
+
+        for (int i = 1; i < 10001; i++) {
+            int n = d(i);
+
+            if (n < 10001) {
+                a[n] = true;
+            }
+        }
+        for (int i = 1; i < 10001; i++) {
+            if (!a[i]) {
+                System.out.println(i);
+            }
+        }
+
+    }
+
+
+    public static int d(int number) {
+        int sum = number;
+        while(number != 0){
+            sum += number%10;
+            number = number/10;
+        }
+        return sum;
     }
 }
