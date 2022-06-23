ddde55ca419db20fad00655d87929bb8e93a73a2
dlawn <dlawnsrjs@gmail.com>
2022-06-23 11:32:18 +0900

    06-23 알고리즘 1181

---
+++ src/java1181.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class java1181 {
+    public static void main(String[] args)throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bf.readLine());
+
+        String[] arr = new String[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i]= bf.readLine();
+        }
+
+        Arrays.sort(arr, (o1 ,o2)-> {
+                if(o1.length()==o2.length()){
+                    return o1.compareTo(o2);
+                }
+                else {
+                    return o1.length() - o2.length();
+                }
+            }
+        );
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(arr[0]).append("\n");
+
+        for (int i = 1; i < N; i++) {
+            if (!arr[i].equals(arr[i-1])){
+                sb.append(arr[i]).append("\n");
+            }
+        }
+        System.out.println(sb);
+
+    }
+}
