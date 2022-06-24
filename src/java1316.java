d795bf11f88f0478976a46b3ad98ec6deeb85e8e
dlawn <dlawnsrjs@gmail.com>
2022-06-24 12:56:32 +0900

    06-24 알고리즘 1316

---
+++ src/java1316.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class java1316 {
+    static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args)throws IOException {
+
+        int N = Integer.parseInt(bf.readLine());
+        int count = 0;
+
+        for (int i = 0; i < N; i++) {
+
+            if(check()==true){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+    public static boolean check()throws IOException{
+        boolean[] check = new boolean[26];
+
+        int prev = 0;
+        String str = bf.readLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            int now = str.charAt(i);
+
+            if(prev!=now){
+
+                if (check[now - 'a'] == false) {
+
+                    check[now -'a'] = true;
+                    prev = now;
+
+                }else {
+                    return false;
+                }
+
+            }
+        }
+        return true;
+    }
+
+}
