56b91c67483a98017d3e46d6c135e827401df3b9
dlawn <dlawnsrjs@gmail.com>
2022-06-24 14:24:05 +0900

    06-24 알고리즘 1427

---
+++ src/java1427.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class java1427 {
+    public static void main(String[] args)throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        char[] arr = bf.readLine().toCharArray();
+
+        Arrays.sort(arr);
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i]);
+        }
+    }
+}
