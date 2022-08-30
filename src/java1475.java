5a5b5fbdc103d1b3fb54b221a187afeafee397d5
dlawnsrjs@gmail.com <dlawnsrjs@gmail.com>
2022-08-31 01:34:54 +0900

    08-31 알고리즘 1475

---
+++ src/java1475.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class java1475 {
+    public static void main(String[] args)throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        int size = str.length();
+        int[] arr = new int[10];
+        int temp = 0;
+        int max = 0;
+
+
+        for (int i = 0; i < size; i++) {
+            temp = str.charAt(i) - '0';
+            arr[temp]++;
+
+        }
+
+        int k = (arr[6] + arr[9]);
+        if (k % 2 == 0) {
+            arr[6] = k / 2;
+            arr[9] = k / 2;
+        }
+        else {
+            arr[6] = k / 2 + 1;
+            arr[9] = k / 2 + 1;
+        }
+        for (int i : arr) {
+            max = Math.max(max, i);
+        }
+        System.out.println(max);
+    }
+}
